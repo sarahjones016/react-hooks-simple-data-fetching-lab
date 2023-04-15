3b36d438ec951a10d7e46e3285e6a13ad8a9f63c
Sarah Jones <sarahrjones8@gmail.com>
2023-04-14 21:41:59 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React, { useState, useEffect } from 'react'
+
+function App() {
+
+  const [dogs, setDogs] = useState(null)
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => res.json())
+    .then((data) => {
+      setDogs(data)
+
+    })
+  }, [])
+
+if (!dogs) {
+  return <p>"Loading..."</p>
+}
+
+  return (
+    <div>
+      <img src={dogs.message} alt="A Random Dog"></img>
+    </div>
+  )
+}
+
+export default App
